7b458ccaf1300477393c537b0046b827a2d82222
Juliandev02 <julian@strawberryfoundations.xyz>
2024-04-03 19:36:33 +0200

    updated locales

--- fields/routes/index.py
+++ fields/routes/index.py
@@ -1,5 +1,11 @@
+from fields.core.locale import get_preferred_language, Strings
 from flask import render_template
 
 
 async def index():
-    return render_template("index.html")
+    language = get_preferred_language()
+    strings = Strings(language)
+
+    return render_template("index.html", **{
+        "strings": strings
+    })
