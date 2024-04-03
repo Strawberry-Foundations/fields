209dd309b45c03782e72f974d046d9d1cc30e7e9
Juliandev02 <julian@strawberryfoundations.xyz>
2024-04-03 18:45:17 +0200

    general app changes

--- __main__.py
+++ __main__.py
@@ -1,9 +1,6 @@
-from fields.core.app import app, config
 from fields.core import Fields
 
-import os
-
-fields = Fields(app, os.path.dirname(os.path.realpath(__file__)))
+fields = Fields()
 
 if __name__ == "__main__":
-    fields.run(config)
+    fields.run()
