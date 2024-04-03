5dd7353de66c403dad0fa8df2038fe3e9c47b451
Juliandev02 <julian@strawberryfoundations.xyz>
2024-04-03 18:57:06 +0200

    Updated config

--- fields/core/config.py
+++ fields/core/config.py
@@ -15,7 +15,6 @@ class FieldsConfig:
         self.debug_mode = config_data["flags"]["debug_mode"]
         self.threaded = config_data["flags"]["threaded"]
 
-        self.mail_tld = config_data["vars"]["mail_tld"]
         self.static_url_path = config_data["vars"]["static_url_path"]
         self.static_folder = config_data["vars"]["static_folder"]
         self.template_folder = config_data["vars"]["template_folder"]
