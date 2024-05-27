467274c8862da1752163a4f6353263b511d99155
Juliandev02 <julian@strawberryfoundations.xyz>
2024-05-27 17:16:24 +0200

    Updated core

--- fields/core/app.py
+++ fields/core/app.py
@@ -3,6 +3,7 @@ from fields.utils.secret import load_secret
 
 from fields.routes.index import index
 from fields.routes.projects.code import all, view
+from fields.routes.spkg import spkg_index
 
 from flask import Flask
 
@@ -16,5 +17,6 @@ app = Flask(
 app.config["SECRET_KEY"] = load_secret()
 
 app.add_url_rule("/", view_func=index)
+app.add_url_rule("/packages", view_func=spkg_index)
 app.add_url_rule("/projects/all/code", view_func=all)
 app.add_url_rule("/projects/<string:project>/code", view_func=view)
