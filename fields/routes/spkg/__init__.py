cb9757958babbfc4807e929ab3cc234b6b9fa1d4
Juliandev02 <julian@strawberryfoundations.xyz>
2024-05-27 17:25:26 +0200

    Updated core

--- fields/routes/spkg/__init__.py
+++ fields/routes/spkg/__init__.py
@@ -1,12 +1,18 @@
 from fields.core.locale import get_preferred_language, Strings
+from fields import root_dir
 from flask import render_template
+
+import os
 
 
 async def spkg_index():
     language = get_preferred_language()
     strings = Strings(language)
 
+    package_dirs = os.listdir(f"{root_dir}/fields_data/packages")
+
     return render_template("spkg/index.html", **{
         "strings": strings,
-        "title": strings.load("spkg_browser")
+        "title": strings.load("spkg_browser"),
+        "package_dirs": package_dirs
     })
