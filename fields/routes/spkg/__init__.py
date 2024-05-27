7c27c158d35a3184c8e9880a45a16d9336290af0
Juliandev02 <julian@strawberryfoundations.xyz>
2024-05-27 17:55:33 +0200

    Updated spkg packages

--- fields/routes/spkg/__init__.py
+++ fields/routes/spkg/__init__.py
@@ -1,4 +1,7 @@
 from fields.core.locale import get_preferred_language, Strings
+from fields.core.spkg.parser import read
+from fields.core.spkg import SpkgBuild
+
 from fields import root_dir
 from flask import render_template
 
@@ -10,9 +13,14 @@ async def spkg_index():
     strings = Strings(language)
 
     package_dirs = os.listdir(f"{root_dir}/fields_data/packages")
+    packages = []
+
+    for package in package_dirs:
+        data = read(f"{root_dir}/fields_data/packages/{package}/compose.spkg")
+        packages.append(data)
 
     return render_template("spkg/index.html", **{
         "strings": strings,
         "title": strings.load("spkg_browser"),
-        "package_dirs": package_dirs
+        "packages": packages
     })
